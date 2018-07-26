7b32a03fd7b1d23c3032fc91c82133919c0ebe6c
Karen Kraft <kraftkj@comcast.net>
2018-07-26 13:53:53 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -7,8 +7,8 @@ def first_steps
     puts "Stop"
       sleep(1)
       break
+  end
 end
-
 # def a_few_more_steps
 #   # Write a loop that outputs the first two sets of steps in the Two-Step
 # end
